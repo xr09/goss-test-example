3c41e1c731641d23df1a6774d8065ed82f87fcbd
Manuel Gutierrez <1380243+xr09@users.noreply.github.com>
2020-12-07 17:02:33 +0100

    Flask sample app

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/hi/<name>')
+def hi(name):
+    return 'Hi, {}!'.format(name)
+
+@app.route('/redir')
+def other():
+    return redirect('https://example.org', code=302)
